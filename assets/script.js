854929d2c263b5446a4795ce24fc0ecaadbd6d5d
David Tunnell <davidtunnell9@gmail.com>
2021-07-02 16:23:53 -0500

    added bootstrap theme, widgets and modal widget popup, live datetime.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
-console.log("test");
+$(document).ready(function() {
+    //get and format current datetime then set it on an 1 sec interval
+    var currentDateTime;
+    setInterval(function() {
+        currentDateTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+        $(".date-time").text(currentDateTime);
+    }, 1000);
+    //set current year
+    $(".current-year").text(moment().format('YYYY'));
+    //open modal on add project button click
+    $('.btn-primary').on('click', function() {
+        $(".modal").dialog({
+            width: 580,
+            height: 635,
+            modal: true
+        });
+    })
+
+});
